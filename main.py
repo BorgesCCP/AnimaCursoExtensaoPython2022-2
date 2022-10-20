0e09921ef5efff8e3715258b56ee33a4caaab531
BorgesCCP <113309346+BorgesCCP@users.noreply.github.com>
2022-10-20 00:53:05 +0000

    estagio: variaveis e impressões

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 # Meu primeiro projeto Python!!!
 # <- comentario
 
+print("-------inicio-------")
 print("Alo Mundo")
+
+# Quando quiser guardar uma String (nome)
+
+nome = "Mateus Borges"
+idade = 21
+
+print(nome)
+print(idade)
+
+mesesvividos = idade*12
+diasvividos = mesesvividos*30
+print(mesesvividos)
+print(diasvividos)
+
